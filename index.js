eebc751d253093284ebf578f7028dd78a54fcb34
Lovisgod <olifedayo94@gmail.com>
2019-10-17 00:40:54 +0100

    FT/added post test and csv fie updated

--- index.js
+++ index.js
@@ -4,43 +4,28 @@ const yergs = require('yargs');
 const { tests } = require('./test');
 
 
-
-// exports.printMsg = function() {
-//     yergs.command({
-//         command:"basic",
-//         describe:"perform basic test",
-//           //here we configure the argurement to be passed into the commands
-//           builder:{
-//             n:{
-//                 describe:'Number of request',
-//                 demandOption: true,
-//                 type:'number'
-//             },
-//             c:{
-//                 describe:'Number of concurrency',
-//                 demandOption: true,
-//                 type:'number'
-//             },
-//             u:{
-//                 describe:'Url to test',
-//                 demandOption: true,
-//                 type:'string'
-//             }
-//         },
-//         handler(argv) {
-//            tests.basicTest(argv.n, argv.c, argv.u);
-//         }
-//     })
-    
-//     //this should be after the commands that you created
-//     //this parse the arguements  with all of the configurations
-//     yergs.parse();
-//   }
-
-exports.basic = function(object) {
+const basic = function(object) {
     const {n, c, u } = object
     return tests.basicTest(n, c, u);
     
-  }
+}
+
+const pxls = function(object) {
+    const {n, c, u } = object;
+    return tests.pxlsTest(n, c, u);
+    
+}  
+
+const pTest = function(object) {
+  const { n, c, u, file, T } = object;
+  return tests.post(n, c, u, file, T);
+}
+
+
+module.exports = {
+  basic,
+  pxls,
+  pTest,
+};
 
   require('make-runnable');
--- test.js
+++ test.js
@@ -7,6 +7,12 @@ const tests = {
     basicTest(n, c, url) {
         shell.exec(`ab -n ${n} -c ${c} -g out.data ${url}`);
       },
+    pxlsTest(n, c, url) {
+      shell.exec(`ab -n ${n} -c ${c} -e result.csv ${url}`);
+    },
+    post(n, c, url, file, T) {
+      shell.exec(`ab -p ${file} -T${T} -n ${n} -c ${c} -e result.csv ${url}`);
+    }
 }
 
 module.exports = {
